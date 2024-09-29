1d81364c0fb10292d16a225e40a31a61d38b3b48
Aleksey Krichevskiy <boolivar@gmail.com>
2024-09-29 22:43:56 +0300

    feat: SpringIntegrationAwsNamespaceHandler element-parser.mapping / META-INF/spring-integration-aws-element-parser.mapping load

--- int-aws-support/src/main/java/org/springframework/integration/aws/config/xml/SpringIntegrationAwsNamespaceHandler.java
+++ int-aws-support/src/main/java/org/springframework/integration/aws/config/xml/SpringIntegrationAwsNamespaceHandler.java
@@ -1,74 +1,34 @@
 package org.springframework.integration.aws.config.xml;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Properties;
 
 public class SpringIntegrationAwsNamespaceHandler extends NamespaceHandlerSupport {
 
-    private final Logger log = LoggerFactory.getLogger(SpringIntegrationAwsNamespaceHandler.class);
-
     @Override
     public void init() {
-        var classLoader = getClass().getClassLoader();
-        var packageName = getClass().getPackageName();
-        try {
-            for (var paths = classLoader.getResources(packageName.replace('.', '/')); paths.hasMoreElements();) {
-                var path = paths.nextElement();
-                try {
-                    Files.list(Path.of(path.toURI()))
-                        .filter(Files::isRegularFile)
-                        .map(file -> file.getFileName().toString())
-                        .filter(fileName -> fileName.endsWith("Parser.class"))
-                        .map(fileName -> loadClass(classLoader, packageName + '.' + fileName.replace(".class", "")))
-                        .filter(BeanDefinitionParser.class::isAssignableFrom)
-                        .forEach(type -> registerBeanDefinitionParser(mapName(type), createParser(type)))
-                        ;
-                } catch (URISyntaxException e) {
-                    log.warn("Error reading path {}", path, e);
-                }
-            }
+        var mapping = new Properties();
+        try (var defaults = getClass().getResourceAsStream("element-parser.mapping")) {
+            mapping.load(defaults);
+            mapping.putAll(PropertiesLoaderUtils.loadAllProperties("META-INF/spring-integration-aws-element-parser.mapping"));
         } catch (IOException e) {
-            throw new UncheckedIOException("Error reading classes for package " + packageName, e);
+            throw new UncheckedIOException("Error load spring-integration-aws element-parser mapping", e);
         }
+        mapping.forEach(this::registerParserMapping);
     }
 
-    private String mapName(Class<?> type) {
-        var typeName = type.getSimpleName();
-        var length = typeName.length() - "Parser".length();
-        var sb = new StringBuilder();
-        sb.append(Character.toLowerCase(typeName.charAt(0)));
-        for (int i = 1; i < length; ++i) {
-            var c = typeName.charAt(i);
-            if (Character.isUpperCase(c)) {
-                sb.append('-').append(Character.toLowerCase(c));
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
-    }
-
-    private Class<?> loadClass(ClassLoader classLoader, String name) {
+    private void registerParserMapping(Object elementName, Object className) {
         try {
-            return classLoader.loadClass(name);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Error load class " + name, e);
-        }
-    }
-
-    private BeanDefinitionParser createParser(Class<?> type) {
-        try {
-            return (BeanDefinitionParser) type.getConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException("Error instantiate parser of type " + type, e);
+            var type = Thread.currentThread().getContextClassLoader().loadClass((String) className);
+            var parser = (BeanDefinitionParser) type.getConstructor().newInstance();
+            registerBeanDefinitionParser((String) elementName, parser);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Error load parser for " + elementName + ": " + className, e);
         }
     }
 }
