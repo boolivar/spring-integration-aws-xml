b1c0d2733fe0c93eee1fe66b52a60533401237ec
Aleksey Krichevskiy <boolivar@gmail.com>
2024-10-07 23:24:35 +0300

    feat: SqsMessageDrivenChannelAdapterParser sync and success-channel attributes, warn on unsupported attributes

--- int-aws-support/src/main/java/org/springframework/integration/aws/config/xml/SqsOutboundChannelAdapterParser.java
+++ int-aws-support/src/main/java/org/springframework/integration/aws/config/xml/SqsOutboundChannelAdapterParser.java
@@ -8,6 +8,8 @@ import org.springframework.integration.aws.outbound.SqsMessageHandler;
 import org.springframework.integration.config.xml.AbstractOutboundChannelAdapterParser;
 import org.w3c.dom.Element;
 
+import java.util.List;
+
 import static org.springframework.integration.config.xml.IntegrationNamespaceUtils.*;
 
 public class SqsOutboundChannelAdapterParser extends AbstractOutboundChannelAdapterParser {
@@ -17,15 +19,37 @@ public class SqsOutboundChannelAdapterParser extends AbstractOutboundChannelAdap
         var builder = BeanDefinitionBuilder.genericBeanDefinition(SqsMessageHandler.class)
             .addConstructorArgReference(element.getAttribute("sqs"));
 
-        setIfAttributeDefined(element, parserContext, builder, "queue");
-        setIfAttributeDefined(element, parserContext, builder, "delay");
-        setIfAttributeDefined(element, parserContext, builder, "message-group-id");
-        setIfAttributeDefined(element, parserContext, builder, "message-deduplication-id");
+        setExpressionValueIfAttributeDefined(builder, element, parserContext, "queue");
+        setExpressionValueIfAttributeDefined(builder, element, parserContext, "delay");
+        setExpressionValueIfAttributeDefined(builder, element, parserContext, "message-group-id");
+        setExpressionValueIfAttributeDefined(builder, element, parserContext, "message-deduplication-id");
+        setExpressionValueIfAttributeDefined(builder, element, parserContext, "send-timeout");
+
+        setReferenceIfAttributeDefined(builder, element, "success-channel", "outputChannelName");
+
+        if (element.hasAttribute("sync")) {
+            var sync = element.getAttribute("sync");
+            if ("true".equalsIgnoreCase(sync)) {
+                builder.addPropertyValue("async", "false");
+            } else if ("false".equalsIgnoreCase(sync)) {
+                builder.addPropertyValue("async", "true");
+            } else {
+                parserContext.getReaderContext()
+                    .error("expected boolean for sync attribute", element);
+            }
+        }
+
+        for (var attribute : List.of("failure-channel", "resource-id-resolver", "error-message-strategy", "async-handler")) {
+            if (element.hasAttribute(attribute)) {
+                parserContext.getReaderContext()
+                    .warning("Attribute" + attribute + " not supported", element);
+            }
+        }
 
         return builder.getBeanDefinition();
     }
-    
-    private void setIfAttributeDefined(Element element, ParserContext parserContext, BeanDefinitionBuilder builder, String attribute) {
+
+    private void setExpressionValueIfAttributeDefined(BeanDefinitionBuilder builder, Element element, ParserContext parserContext, String attribute) {
         if (element.hasAttribute(attribute) && element.hasAttribute(attribute + "-expression")) {
             parserContext.getReaderContext()
                 .error(attribute + " and " + attribute + "-expression attributes are mutually exclusive", element);
