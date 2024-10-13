e1c543c9e4b4352d0bb04afad4c49ab01733e74a
Aleksey Krichevskiy <boolivar@gmail.com>
2024-10-13 21:26:23 +0300

    feat: S3InboundStreamingChannelAdapterParser to parse s3-inbound-streaming-channel-adapter

---
+++ int-aws-support/src/main/java/org/springframework/integration/aws/config/xml/S3InboundStreamingChannelAdapterParser.java
@@ -0,0 +1,36 @@
+package org.springframework.integration.aws.config.xml;
+
+import org.springframework.integration.aws.inbound.S3StreamingMessageSource;
+import org.springframework.integration.aws.support.S3RemoteFileTemplate;
+import org.springframework.integration.aws.support.filters.S3PersistentAcceptOnceFileListFilter;
+import org.springframework.integration.aws.support.filters.S3RegexPatternFileListFilter;
+import org.springframework.integration.aws.support.filters.S3SimplePatternFileListFilter;
+import org.springframework.integration.file.config.AbstractRemoteFileStreamingInboundChannelAdapterParser;
+
+public class S3InboundStreamingChannelAdapterParser extends AbstractRemoteFileStreamingInboundChannelAdapterParser {
+
+    @Override
+    protected Class<S3StreamingMessageSource> getMessageSourceClass() {
+        return S3StreamingMessageSource.class;
+    }
+
+    @Override
+    protected Class<S3RemoteFileTemplate> getTemplateClass() {
+        return S3RemoteFileTemplate.class;
+    }
+
+    @Override
+    protected Class<S3SimplePatternFileListFilter> getSimplePatternFileListFilterClass() {
+        return S3SimplePatternFileListFilter.class;
+    }
+
+    @Override
+    protected Class<S3RegexPatternFileListFilter> getRegexPatternFileListFilterClass() {
+        return S3RegexPatternFileListFilter.class;
+    }
+
+    @Override
+    protected Class<S3PersistentAcceptOnceFileListFilter> getPersistentAcceptOnceFileListFilterClass() {
+        return S3PersistentAcceptOnceFileListFilter.class;
+    }
+}
