ef668a4f51a6a3d130465e02d1e9f534a99d09d3
Aleksey Krichevskiy <boolivar@gmail.com>
2024-11-02 00:18:22 +0200

    feat: SqsOutboundChannelAdapterParser

---
+++ int-aws/src/main/java/org/springframework/integration/aws/config/xml/SqsOutboundChannelAdapterParser.java
@@ -0,0 +1,26 @@
+package org.springframework.integration.aws.config.xml;
+
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.integration.aws.outbound.SqsMessageHandler;
+import org.springframework.integration.config.xml.AbstractOutboundChannelAdapterParser;
+import org.w3c.dom.Element;
+
+public class SqsOutboundChannelAdapterParser extends AbstractOutboundChannelAdapterParser {
+
+    @Override
+    protected AbstractBeanDefinition parseConsumer(Element element, ParserContext parserContext) {
+        return XmlBeanDefinitionBuilder.newInstance(element, parserContext, SqsMessageHandler.class)
+            .addConstructorArgReference("sqs")
+            .setPropertyOrExpressionStringIfAttributeDefined("queue")
+            .setPropertyOrExpressionStringIfAttributeDefined("delay")
+            .setPropertyOrExpressionStringIfAttributeDefined("message-group-id")
+            .setPropertyOrExpressionStringIfAttributeDefined("message-deduplication-id")
+            .setPropertyOrExpressionStringIfAttributeDefined("send-timeout")
+            .setPropertyIfAttributeDefined("async")
+            .setPropertyIfAttributeDefined("queue-not-found-strategy")
+            .setPropertyIfAttributeDefined("output-channel", "outputChannelName")
+            .setPropertyReferenceIfAttributeDefined("message-converter")
+            .build();
+    }
+}
