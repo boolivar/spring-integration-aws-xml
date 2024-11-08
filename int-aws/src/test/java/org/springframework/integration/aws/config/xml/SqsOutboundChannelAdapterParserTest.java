f168dc671bf10c4508e0337f10c4e347a2303fc7
Aleksey Krichevskiy <boolivar@gmail.com>
2024-11-08 22:18:01 +0200

    refactor: SqsOutboundChannelAdapterParserTest refactored using mockConstruction

--- int-aws/src/test/java/org/springframework/integration/aws/config/xml/SqsOutboundChannelAdapterParserTest.java
+++ int-aws/src/test/java/org/springframework/integration/aws/config/xml/SqsOutboundChannelAdapterParserTest.java
@@ -4,7 +4,6 @@ import io.awspring.cloud.sqs.listener.QueueNotFoundStrategy;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.integration.aws.outbound.SqsMessageHandler;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.converter.MessageConverter;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
@@ -18,43 +17,39 @@ class SqsOutboundChannelAdapterParserTest extends ParserTestBase {
     @Mock
     private MessageConverter messageConverter;
 
-    @Mock
-    private MessageChannel out;
-
     @Test
     void testParser() {
         registerBean("sqs", SqsAsyncClient.class, sqs);
         registerBean("mc", MessageConverter.class, messageConverter);
-        when(channelResolver.resolveDestination("out"))
-            .thenReturn(out);
 
-        var handler = loadBean(SqsMessageHandler.class, """
-            <int-aws:sqs-outbound-channel-adapter sqs="sqs"
-                    channel="in"
-                    async="#{true}"
-                    delay="#{50}"
-                    queue="#{'q'}"
-                    order="#{5}"
-                    output-channel="#{'out'}"
-                    queue-not-found-strategy="#{'CREATE'}"
-                    send-timeout="#{50}"
-                    message-deduplication-id="#{'dd'}"
-                    message-group-id="#{'mg'}"
-                    message-converter="mc"/>
-            """);
+        try (var mocked = mockConstruction(SqsMessageHandler.class, (mock, context) -> assertThat(context.arguments().get(0)).isSameAs(sqs))) {
+            var handler = loadBean(SqsMessageHandler.class, """
+                <int-aws:sqs-outbound-channel-adapter sqs="sqs"
+                        channel="in"
+                        async="#{true}"
+                        delay="#{50}"
+                        queue="#{'q'}"
+                        order="#{5}"
+                        output-channel="#{'out'}"
+                        queue-not-found-strategy="#{'CREATE'}"
+                        send-timeout="#{50}"
+                        message-deduplication-id="#{'dd'}"
+                        message-group-id="#{'mg'}"
+                        message-converter="mc"/>
+                """);
 
-        assertThat(handler)
-            .returns(out, SqsMessageHandler::getOutputChannel)
-            .returns(5, SqsMessageHandler::getOrder)
-            .hasFieldOrPropertyWithValue("amazonSqs", sqs)
-            .hasFieldOrPropertyWithValue("messageConverter", messageConverter)
-            .hasFieldOrPropertyWithValue("async", true)
-            .hasFieldOrPropertyWithValue("queueNotFoundStrategy", QueueNotFoundStrategy.CREATE)
-            .hasFieldOrPropertyWithValue("queueExpression.value", "q")
-            .hasFieldOrPropertyWithValue("delayExpression.value", 50)
-            .hasFieldOrPropertyWithValue("sendTimeoutExpression.value", 50L)
-            .hasFieldOrPropertyWithValue("messageGroupIdExpression.value", "mg")
-            .hasFieldOrPropertyWithValue("messageDeduplicationIdExpression.value", "dd")
-            ;
+            assertThat(mocked.constructed()).size().isOne();
+
+            verify(handler).setAsync(true);
+            verify(handler).setDelay(50);
+            verify(handler).setQueue("q");
+            verify(handler).setOrder(5);
+            verify(handler).setOutputChannelName("out");
+            verify(handler).setQueueNotFoundStrategy(QueueNotFoundStrategy.CREATE);
+            verify(handler).setSendTimeout(50);
+            verify(handler).setMessageDeduplicationId("dd");
+            verify(handler).setMessageGroupId("mg");
+            verify(handler).setMessageConverter(messageConverter);
+        }
     }
 }
