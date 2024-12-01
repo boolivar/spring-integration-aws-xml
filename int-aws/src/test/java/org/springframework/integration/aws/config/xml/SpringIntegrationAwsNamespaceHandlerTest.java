6a2fdc3a133585a638345f094a27e98970a279e9
Aleksey Krichevskiy <boolivar@gmail.com>
2024-12-02 00:21:01 +0200

    test(int-aws): SpringIntegrationAwsNamespaceHandlerTest

---
+++ int-aws/src/test/java/org/springframework/integration/aws/config/xml/SpringIntegrationAwsNamespaceHandlerTest.java
@@ -0,0 +1,36 @@
+package org.springframework.integration.aws.config.xml;
+
+import org.assertj.core.api.Condition;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpringIntegrationAwsNamespaceHandlerTest {
+
+    private final SpringIntegrationAwsNamespaceHandler handler = new SpringIntegrationAwsNamespaceHandler();
+
+    @Test
+    void test() {
+        handler.init();
+        assertThat(handler)
+            .extracting("parsers", InstanceOfAssertFactories.MAP)
+            .hasEntrySatisfying("kcl-message-driven-channel-adapter", instanceOf(KclMessageDrivenChannelAdapterParser.class))
+            .hasEntrySatisfying("kinesis-message-driven-channel-adapter", instanceOf(KinesisMessageDrivenChannelAdapterParser.class))
+            .hasEntrySatisfying("kinesis-outbound-channel-adapter", instanceOf(KinesisOutboundChannelAdapterParser.class))
+            .hasEntrySatisfying("kpl-outbound-channel-adapter", instanceOf(KplOutboundChannelAdapterParser.class))
+            .hasEntrySatisfying("s3-inbound-channel-adapter", instanceOf(S3InboundChannelAdapterParser.class))
+            .hasEntrySatisfying("s3-inbound-streaming-channel-adapter", instanceOf(S3InboundStreamingChannelAdapterParser.class))
+            .hasEntrySatisfying("s3-outbound-channel-adapter", instanceOf(S3OutboundChannelAdapterParser.class))
+            .hasEntrySatisfying("s3-outbound-gateway", instanceOf(S3OutboundGatewayParser.class))
+            .hasEntrySatisfying("sns-inbound-channel-adapter", instanceOf(SnsInboundChannelAdapterParser.class))
+            .hasEntrySatisfying("sns-outbound-channel-adapter", instanceOf(SnsOutboundChannelAdapterParser.class))
+            .hasEntrySatisfying("sqs-outbound-channel-adapter", instanceOf(SqsOutboundChannelAdapterParser.class))
+            .hasEntrySatisfying("sqs-message-driven-channel-adapter", instanceOf(SqsMessageDrivenChannelAdapterParser.class))
+            ;
+    }
+
+    private Condition<Object> instanceOf(Class<?> type) {
+        return new Condition<>(type::isInstance, "instance of " + type);
+    }
+}
